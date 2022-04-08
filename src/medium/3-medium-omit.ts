bcc5f40e72678ad48d9139657e34410b81ef1ffc
aloha66 <lzcbruce@live.com>
2022-04-08 23:34:18 +0800

    feat: finish omit

---
+++ src/medium/3-medium-omit.ts
@@ -0,0 +1,70 @@
+/*
+  3 - Omit
+  -------
+  by Anthony Fu (@antfu) #medium #union #built-in
+
+  ### Question
+
+  Implement the built-in `Omit<T, K>` generic without using it.
+
+  Constructs a type by picking all properties from `T` and then removing `K`
+
+  For example
+
+  ```ts
+  interface Todo {
+    title: string
+    description: string
+    completed: boolean
+  }
+
+  type TodoPreview = MyOmit<Todo, 'description' | 'title'>
+
+  const todo: TodoPreview = {
+    completed: false,
+  }
+  ```
+
+  > View on GitHub: https://tsch.js.org/3
+*/
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+/* _____________ Your Code Here _____________ */
+
+// todo 写注释
+type MyExclude<T, K> = T extends K ? never : T
+type MyOmit<T, K extends keyof T> = {
+  [P in MyExclude<keyof T, K>]: T[P]
+}
+
+export type cases = [
+  Expect<Equal<Expected1, MyOmit<Todo, 'description'>>>,
+  Expect<Equal<Expected2, MyOmit<Todo, 'description' | 'completed'>>>,
+]
+
+// @ts-expect-error
+type error = MyOmit<Todo, 'description' | 'invalid'>
+
+interface Todo {
+  title: string
+  description: string
+  completed: boolean
+}
+
+interface Expected1 {
+  title: string
+  completed: boolean
+}
+
+interface Expected2 {
+  title: string
+}
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/3/answer
+  > View solutions: https://tsch.js.org/3/solutions
+  > More Challenges: https://tsch.js.org
+*/
