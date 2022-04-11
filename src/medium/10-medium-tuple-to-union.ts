57261940b5cb62e78fd7bc9e2b61376d0de5a64d
aloha66 <lzcbruce@live.com>
2022-04-11 21:57:29 +0800

    feat: finish TupleToUnion

---
+++ src/medium/10-medium-tuple-to-union.ts
@@ -0,0 +1,42 @@
+/*
+  10 - Tuple to Union
+  -------
+  by Anthony Fu (@antfu) #medium #infer #tuple #union
+
+  ### Question
+
+  Implement a generic `TupleToUnion<T>` which covers the values of a tuple to its values union.
+
+  For example
+
+  ```ts
+  type Arr = ['1', '2', '3']
+
+  type Test = TupleToUnion<Arr> // expected to be '1' | '2' | '3'
+  ```
+
+  > View on GitHub: https://tsch.js.org/10
+*/
+
+/**
+ * 知识点
+ * T[number] 遍历数组转换成联合类型
+ */
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+/* _____________ Your Code Here _____________ */
+type TupleToUnion<T extends any[]> = T[number]
+
+export type cases = [
+  Expect<Equal<TupleToUnion<[123, '456', true]>, 123 | '456' | true>>,
+  Expect<Equal<TupleToUnion<[123]>, 123>>,
+]
+
+/* _____________ Further Steps _____________ */
+/*
+  > Share your solutions: https://tsch.js.org/10/answer
+  > View solutions: https://tsch.js.org/10/solutions
+  > More Challenges: https://tsch.js.org
+*/
